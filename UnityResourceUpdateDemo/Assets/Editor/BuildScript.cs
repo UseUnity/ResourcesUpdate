c0d9497d36b986ac627ca2b04b5b9b5ad4c653bf
LimengGame <1158096230@qq.com>
2018-04-23 20:08:38 +0800

    Editor

---
+++ UnityResourceUpdateDemo/Assets/Editor/BuildScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildScript {
+
+    [MenuItem("Tools/Build Assetbundles", false)]
+    public static void BuildActivePlatformAssetbundles()
+    {
+        BuildAssetbundles(EditorUserBuildSettings.activeBuildTarget);
+    }
+
+    public static void BuildAssetbundles(BuildTarget targetPlatform)
+    {
+        string inputPath = "Assets/Data";
+        string outputPath = "Assets/StreamingAssets/" + GetPlatformName(Application.platform);
+
+        if (Directory.Exists(outputPath) == false)
+            Directory.CreateDirectory(outputPath);
+        List<AssetBundleBuild> builds = CollectAssetbundleBuilds(inputPath, outputPath);
+        BuildAssetBundleOptions options = BuildAssetBundleOptions.DeterministicAssetBundle | 
+                                            BuildAssetBundleOptions.ChunkBasedCompression | 
+                                            BuildAssetBundleOptions.DisableWriteTypeTree;
+        BuildPipeline.BuildAssetBundles(outputPath, builds.ToArray(), options, targetPlatform);
+    }
+
+    private static List<AssetBundleBuild> CollectAssetbundleBuilds(string inputPath, string outputPath)
+    {
+        List<AssetBundleBuild> builds = new List<AssetBundleBuild>();
+        CollectSingleFileBuilds(inputPath, "Atlas/", "*.prefab", SearchOption.AllDirectories, outputPath, builds);
+        CollectSingleFileBuilds(inputPath, "Config/", "*.text", SearchOption.TopDirectoryOnly, outputPath, builds);
+        CollectSingleFileBuilds(inputPath, "Shader/", "*.shader", SearchOption.TopDirectoryOnly, outputPath, builds);
+
+        return builds;
+    }
+
+    private static void CollectSingleFileBuilds(string rootPath, string searchSubPath, string filePattern, SearchOption searchOption, string outputPath, List<AssetBundleBuild> builds)
+    {
+        string[] files = Directory.GetFiles(rootPath + searchSubPath, filePattern, searchOption);
+        for (int i = 0; i < files.Length; i++)
+        {
+            string filePath = files[i].ToLower();
+            string filePath2 = string.Empty;
+            AssetBundleBuild build = new AssetBundleBuild();
+            build.assetBundleName = filePath.Substring(rootPath.Length, filePath.Length - rootPath.Length) + ".ab";
+            if (filePattern == "*.shader" && filePath.Contains("standardinst"))
+            {
+                build.assetNames = new string[2];
+                build.assetNames[0] = filePath2;
+                build.assetNames[1] = filePath;
+            }
+            else
+            {
+                build.assetNames = new string[1];
+                build.assetNames[0] = filePath;
+            }
+            builds.Add(build);
+        }
+    }
+
+    public static string GetPlatformName(RuntimePlatform platform)
+    {
+        switch (platform)
+        {
+            case RuntimePlatform.Android:
+                return "android";
+            case RuntimePlatform.IPhonePlayer:
+                return "ios";
+            case RuntimePlatform.WindowsEditor:
+            case RuntimePlatform.WindowsPlayer:
+                return "windows";
+            case RuntimePlatform.OSXEditor:
+            case RuntimePlatform.OSXPlayer:
+                return "osx";
+            default:
+                return "others";
+        }
+    }
+
+}
---
+++ UnityResourceUpdateDemo/Assets/Editor/ExportVersion.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+public class ExportVersion : MonoBehaviour {
+    [MenuItem("Tools/ExportVersion")]
+    public static void ExportVersionAndMD5()
+    {
+        string resourcePath = Application.streamingAssetsPath + "/" + BuildScript.GetPlatformName(Application.platform) + "/";
+        StringBuilder versions = new StringBuilder();
+        string[] files = Directory.GetFiles(resourcePath, "*", SearchOption.AllDirectories);
+        for (int i = 0; i < files.Length; i++)
+        {
+            string filePath = files[i];
+            if (!filePath.Contains("."))
+            {
+                string relativePath = filePath.Replace(resourcePath, "").Replace("\\", "/");
+                string md5 = MD5File(relativePath);
+                versions.Append(relativePath).Append(",").Append(md5).Append("\n");
+                continue;
+            }
+            string extension = filePath.Substring(filePath.LastIndexOf("."));
+            if (extension == ".ab")
+            {
+                string relativePath = filePath.Replace(resourcePath, "").Replace("\\", "/");
+                string md5 = MD5File(relativePath);
+                versions.Append(relativePath).Append(",").Append(md5).Append("\n");
+            }
+        }
+        FileStream stream = new FileStream(resourcePath + "version.txt", FileMode.Create);
+        byte[] data = Encoding.UTF8.GetBytes(versions.ToString());
+        stream.Write(data, 0, data.Length);
+        stream.Flush();
+        stream.Close();
+    }
+
+    public static string MD5File(string file)
+    {
+        try
+        {
+            FileStream stream = new FileStream(file, FileMode.Open);
+            System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] retValue = md5.ComputeHash(stream);
+            stream.Close();
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < retValue.Length; i++)
+            {
+                str.Append(retValue[i].ToString("x2"));
+            }
+            return str.ToString();
+        }
+        catch (System.Exception e)
+        {
+            throw new System.Exception("md5file() fail, error:" + e.Message);
+        }
+    }
+
+}
